8f542a4ff7e8526f826da882b3baf85468340067
masonrandall <mason@blueplate.ai>
2025-04-16 09:43:34 -0700

    -GH initial

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { registerPlugin } from '@capacitor/core';
 import type { StarPlugin } from './definitions';
 
 const Star = registerPlugin<StarPlugin>('Star', {
-  web: () => import('./web').then((m) => new m.StarWeb()),
 });
 
 export * from './definitions';
--- src/web.ts
+++
@@ -1,9 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-
-import type { StarPlugin } from './definitions';
-
-export class StarWeb extends WebPlugin implements StarPlugin {
-  async print(options: { value: string }): Promise<{ success: boolean }> {
-    return { success: false };
-  }
-}
