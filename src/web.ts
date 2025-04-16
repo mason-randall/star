7ea508b21dcb9bfb15f24c9160df91ccb120994c
masonrandall <mason@blueplate.ai>
2025-04-16 09:22:01 -0700

    -GH initial

--- src/web.ts
+++ src/web.ts
@@ -4,6 +4,6 @@ import type { StarPlugin } from './definitions';
 
 export class StarWeb extends WebPlugin implements StarPlugin {
   async print(options: { value: string }): Promise<{ success: boolean }> {
-    return false;
+    return { success: false };
   }
 }
